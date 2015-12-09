05b7b577745b046671242e6967395e85b7c6698a
Robilemb <iwazaru45@hotmail.fr>
2015-12-09 23:30:32 +0100

    Création GBCE Windows
    Création du projet VS.

---
+++ GBCE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Windows.h>
+
+#define SLEEPTIME 1000000000
+
+int main(int argc, char** argv)
+{
+	std::cout << "This is GBCE V0.0000001 !" << std::endl;
+	std::cout << "Copyright 2015 - JP. BENTEJAC - R. LEMBACH" << std::endl;
+
+	Sleep(SLEEPTIME);
+
+	return 0;
+}
