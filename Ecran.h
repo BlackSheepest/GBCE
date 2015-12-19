0e4dbbb7b64ec37e6856d0d601f94d95e31234e1
Robilemb <iwazaru45@hotmail.fr>
2015-12-19 22:15:39 +0100

    Ouverture fenêtre vide
    Ouverture d'une fenêtre vide SDL2

--- Ecran.h
+++ Ecran.h
@@ -4,6 +4,20 @@
 #include <iostream>
 #include "SDL.h"
 
+// Nombre de couleur affichable par l'écran en puissance de 2 (2^15)
+#define ECRAN_NB_COULEURS 15
+
+// Dimension de l'écran de la GBC
+#define ECRAN_LARGEUR_BASE 160
+#define ECRAN_HAUTEUR_BASE 144
+
+// Facteur d'agrandissement de la taile des pixels
+#define ECRAN_DIMPIXEL 2
+
+// Dimension de l'afficheur SDL
+#define ECRAN_LARGEUR  ECRAN_LARGEUR_BASE*ECRAN_DIMPIXEL
+#define ECRAN_HAUTEUR  ECRAN_HAUTEUR_BASE*ECRAN_DIMPIXEL
+
 class Ecran
 {
 public:
--- GBCE.cpp
+++ GBCE.cpp
@@ -2,19 +2,71 @@
 #include <Windows.h>
 #include "SDL.h"
 
-#define SLEEPTIME 1000000000
+#include "Ecran.h"
+#include "Cpu.h"
 
+void close();
+
+// Fonction Main
+// *************
 int main(int argc, char** argv)
 {
-	// INITIALISATION DE SDL
+	// INITIALISATION DE SDL2
 	SDL_Init(SDL_INIT_EVERYTHING);
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		std::cout << "Erreur d'initialisation de la SDL : " << SDL_GetError() << std::endl;
+		exit(EXIT_FAILURE);
+	}
 
-	std::cout << "Execution de l'application SDL2" << std::endl;
+	// CREATION DE LA FENETRE PRINCIPALE
+	SDL_Window *ecran = SDL_CreateWindow(
+		"GBCE", 
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		ECRAN_LARGEUR,
+		ECRAN_HAUTEUR,
+		SDL_WINDOW_SHOWN);
+
+	if (ecran == NULL)
+	{
+		std::cout << "Erreur lors de la création de la fenêtre principale : " << SDL_GetError() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// AFFICHAGE DE LA FENETRE PRINCIPALE
+	SDL_UpdateWindowSurface(ecran);
+
+	//SDL_Renderer *renderer = SDL_CreateRenderer(ecran, -1, 0);
+	//SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear"); => SDL_WINDOW_FULLSCREEN_DESKTOP
+	//SDL_RenderSetLogicalSize(renderer, ECRAN_LARGEUR, ECRAN_HAUTEUR);
+	//SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+	close();
+
+	// DESTRUCTION DE LA FENETRE PRINCIPALE
+	SDL_DestroyWindow(ecran);
 
 	// FERMETURE DE SDL
 	SDL_Quit();
 
-	Sleep(SLEEPTIME);
+	return EXIT_SUCCESS;
+}
 
-	return 0;
+// Gestion de la fermeture du programme
+// ************************************
+void close()
+{
+	int continuer = 1;
+	SDL_Event event;
+
+	while (continuer)
+	{
+		SDL_WaitEvent(&event);
+		switch (event.type)
+		{
+			case SDL_QUIT:
+				continuer = 0;
+		}
+	}
 }
